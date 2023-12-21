19ed8400ed0c589baf42f59c31c595783df710c1
amiadud <sopnilstar@gmail.com>
2023-12-22 01:09:30 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,122 @@
+const express = require('express')
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const cors = require('cors')
+const app = express()
+const port = process.env.PORT || 5000;
+
+//dot environment config
+require('dotenv').config()
+
+//middlewares
+app.use(express.json());
+app.use(cors({
+    origin: [
+    "http://localhost:5173",
+    "https://task-management-one-sepia.vercel.app"
+  ],
+    credentials:true
+  }));
+
+// TaskManagement
+// oVwCB9m9zaQw2Yz4
+  
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.35itrev.mongodb.net/?retryWrites=true&w=majority`;
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+
+    const taskCollection = client.db("TaskManagement").collection("tasks");
+
+    app.get('/', (req, res) => {
+        res.send('Welcome to my server')
+        })
+    
+    app.post('/tasks', async (req, res) => {
+    const newTask = req.body;
+    await taskCollection.insertOne(newTask);
+    res.send(newTask);
+    });
+
+    app.get('/tasks', async (req, res) => {
+      try {
+        const email = req.query.email
+        const query = {email: email}
+        const tasks = await taskCollection.find(query).toArray();
+        res.send(tasks);
+      } catch (error) {
+        console.error('Error fetching tasks:', error);
+        res.status(500).json({ message: 'Internal Server Error' });
+      }
+    });
+//update task 
+app.patch('/tasks/:id', async(req, res) => {
+  try{
+    const id = req.params.id;
+    const updateItem = req.body
+    const filter = { _id: new ObjectId (id) }
+    const updateDoc = {
+      $set: {
+        title: updateItem.petName,
+        description: updateItem.petAge,
+        petLocation: updateItem.petLocation,
+        priority: updateItem.image,
+        deadline: new Date().toDateString(),
+      }
+    }
+    const result = await taskCollection.updateOne(filter, updateDoc )
+    res.send(result);
+
+  }catch(err){
+      console.log(err.message);
+  }
+})
+
+app.put('/tasks/:id', async (req, res) => {
+  const taskId = req.params.id;
+
+  try {
+    const updatedTask = await taskCollection.findOneAndUpdate(
+      { _id: new ObjectId(taskId) },
+      { $set: req.body },
+      { returnDocument: 'after' }
+    );
+
+    res.json(updatedTask.value);
+  } catch (error) {
+    console.error('Error updating task:', error);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+});
+
+    app.delete('/tasks/:id', async (req, res) => {
+      const taskId = req.params.id;
+      try {
+        const result = await taskCollection.deleteOne({ _id: new ObjectId(taskId) });
+        res.send(result);
+      } catch (error) {
+        console.error('Error deleting task:', error);
+        res.status(500).json({ message: 'Internal Server Error' });
+      }
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
